065d889c7f3734cfba39e81335f8638888ab53a2
AqielMubarok <aqiel.upi.edu@upi.edu>
2024-12-10 21:10:42 +0700

    Membuat Halaman Ganti Password

---
+++ resources/views/pages/auth/auth-ganti-password.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.auth')
+
+@section('title', 'Ganti Password')
+
+@push('style')
+    <!-- CSS Libraries -->
+@endpush
+
+@section('main')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Ganti Password</h4>
+        </div>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+            @endif
+        <div class="card-body">
+            <p class="text-muted">We will send a link to reset your password</p>
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email"
+                        type="email"
+                        class="form-control @error ('email') is-invalid @enderror"
+                        name="email"
+                        value="{{ old('email') }}"
+                        required autocomplete="email"
+                        utofocus
+                        placeholder="Masukkan Alamat Email">
+                        @error('email')
+                </div>
+                <div class="alert alert-danger mt-2">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    @enderror
+                <div class="form-group">
+                    <button type="submit"
+                        class="btn btn-primary btn-lg btn-block"
+                        tabindex="4">
+                        Ganti Password
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+@endpush
