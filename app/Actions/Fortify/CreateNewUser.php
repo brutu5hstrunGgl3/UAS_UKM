c3b92933ee04893cfcb464d7232473a3165598db
AqielMubarok <aqiel.upi.edu@upi.edu>
2024-12-10 21:01:04 +0700

    Menambah data nomor handphone

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -28,6 +28,7 @@ class CreateNewUser implements CreatesNewUsers
             abort(403, 'Pengguna sudah melebihi batas kuota');
         }
 
+        // Validasi input termasuk nomor handphone
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
@@ -37,17 +38,30 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
+            'phone' => [
+                'required',
+                'string',
+                'max:15',
+                'regex:/^\+?[0-9]{10,15}$/', // Format nomor telepon
+                Rule::unique(User::class),
+            ],
+
             'password' => $this->passwordRules(),
         ], [
+            'phone.required' => 'Nomor handphone diperlukan.',
+            'phone.regex' => 'Nomor handphone tidak valid.',
+            'phone.unique' => 'Nomor handphone sudah terdaftar.',
             'password.required' => 'Password diperlukan.',
             'password.min' => 'Password harus memiliki minimal 8 karakter.',
             'password.regex' => 'Password harus mengandung huruf kecil, huruf besar, angka, dan simbol.',
             'password.confirmed' => 'Konfirmasi password tidak cocok.',
         ])->validate();
 
+        // Menyimpan pengguna baru
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'phone' => $input['phone'], 
             'password' => Hash::make($input['password']),
         ]);
     }
